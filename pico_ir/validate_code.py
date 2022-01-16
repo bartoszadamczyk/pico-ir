a48b21c83f93938d6277c9f0946287da5bb49860
Bartosz Adamczyk <3093076+bartoszadamczyk@users.noreply.github.com>
2022-01-16 03:08:13 +0000

    Rename comments

--- pico_ir/validate_code.py
+++ pico_ir/validate_code.py
@@ -18,13 +18,13 @@ def validate_code(code):
         raise CodeTooLong
 
     # check 8-bit device address
-    # following 8-bits should be a logical inverse of the device address
+    # following 8-bits have to be a logical inverse of the device address
     for i in range(0, 8):
         if code[i] == code[i + 8]:
             raise CodeInvalid
 
     # check 8-bit command
-    # following 8-bits should be a logical inverse of the command
+    # following 8-bits have to be a logical inverse of the command
     for i in range(16, 24):
         if code[i] == code[i + 8]:
             raise CodeInvalid
